a26d1f946b70a406aa6edf24164cf420bffe7548
Jubin khatri <jubinkhatriii12@gmail.com>
2026-03-30 01:38:23 +0545

    Update link in hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1O97WCk2DrO9x6SHOqf7LvRbmHkMgGIb4/view?usp=sharing"
+                    "https://drive.google.com/file/d/1EjQ5JgmBIsYxCiqtG_CAmGPcRkNNWMd9/view?usp=drivesdk"
                   }
                   target="_blank"
                   className="flex-1"

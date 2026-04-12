26c06dc4fa9ee4961947f5dfc08749db94835f2c
Jubin khatri <jubinkhatriii12@gmail.com>
2026-04-13 00:01:29 +0545

    Update config for developer name and GitHub details

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,7 +3,7 @@ const config = {
   description: {
     long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
     short:
-      "Discover the portfolio of Naresh, a full-stack developer creating interactive web experiences and innovative projects.",
+      "Discover the portfolio of Jubin, a full-stack developer creating interactive web experiences and innovative projects.",
   },
   keywords: [
     "Naresh",
@@ -28,8 +28,8 @@ const config = {
   site: "https://nareshkhatri.site",
 
   // for github stars button
-  githubUsername: "naresh-khatri",
-  githubRepo: "3d-portfolio",
+  githubUsername: "jubinkhatriiiiiiiiii",
+  githubRepo: "socket",
 
   get ogImg() {
     return this.site + "/assets/seo/og-image.png";
